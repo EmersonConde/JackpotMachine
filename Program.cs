7c78917b816c953dc4e70ab5ed8273eecb5dce2f
Émerson Conde <emersonsilvacaucaia27@gmail.com>
2024-11-27 21:14:33 +0000

    Initial Structure

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace JackpotMachine
+{
+    class Program
+    {
+        static void Main()
+        {
+            string roll = "no";
+            while  (roll == "no")
+            {
+            Console.Clear();
+            Console.WriteLine("Hello Player, are you ready to be rich or poor, your luck decides 💎");
+            Console.WriteLine("Do you want to roll? (yes/no)");
+            roll = Console.ReadLine();
+            if (roll != "yes"  && roll != "no")
+            {
+                roll = "no";
+            }
+            else if (roll == "yes") {
+                rollmachine();
+            };
+            };
+        }
+        static void rollmachine()
+        {
+            Console.WriteLine("Rolling Done! ✨");
+        }
+    }
+}
